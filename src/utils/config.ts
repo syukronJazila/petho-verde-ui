b83cae80f92d6d088ae01387bfc21e2cfd0a1fcf
syukronJazila <jazilasyukron12@gmail.com>
2025-10-11 14:48:38 +0700

    perbaiki api 4

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "/api/proxy?path=";
+// export const BASE_URL = "/api/proxy?path=";
 // export const BASE_URL = "https://corsproxy.io/?" + encodeURIComponent("https://herbal-ajie.free.nf/pethofar");
-// export const BASE_URL = "https://herbal-ajie.free.nf/pethofar";
+export const BASE_URL = "https://herbal-ajie.free.nf/pethofar/";
 // export const BASE_URL = "http://localhost/pethofar";
